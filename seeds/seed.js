76e1723bacfa6eb09ff85ade79ecc8a5bfb5c0d2
Dian Sherman <slayer_barrett_@hotmail.com>
2023-02-06 14:37:22 -0800

    added seed

---
+++ seeds/seed.js
@@ -0,0 +1,32 @@
+const sequelize = require('../config/connection');
+const { User, Dahlia } = require("../models")
+const seed = async () => {
+    await sequelize.sync({ force: true })
+    const users = [
+        {
+            email: "dian@dian.dian",
+            password: "password"
+        }
+    ]
+    const dahlias = [
+        {
+            name: "Super",
+            primaryColor: "blue",
+            secondaryColor: "red",
+            size: 8,
+            have: yes,
+            want: no
+        }
+    ]
+    try {
+
+        await User.bulkCreate(users, {
+            individualHooks: true
+        })
+        await Dahlia.bulkCreate(dahlias)
+    } catch (err) {
+        throw err
+    }
+    process.exit(0);
+}
+seed()
