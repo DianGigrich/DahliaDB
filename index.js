a8bc34f1db33830557616f046a37714cbce180f0
Dian Sherman <slayer_barrett_@hotmail.com>
2023-02-28 14:25:19 -0800

    added sessions

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const express = require('express');
-
+const session = require('express-session');
 const allRoutes = require('./controllers');
-
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const cors = require("cors");
 
-const cors = require("cors")
 app.use(cors())
 // Sets up the Express App
 // =============================================================
 const app = express();
-
 const PORT = process.env.PORT || 3001;
-// Requiring our models for syncing
-const { User,Dahlia} = require('./models');
+
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      maxAge:1000*60*60*2
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
 
 // Sets up the Express app to handle data parsing
+
+app.use(session(sess));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
